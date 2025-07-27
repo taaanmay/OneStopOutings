9c50cf02c51f9a3951ef6d0bdede8145ff76e8f1
Tanmay Kaushik <tanmaykaushik2015@gmail.com>
2025-07-27 23:24:22 +0100

    Changig vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Ensures relative paths in HTML & assets (important for Vercel)
+  build: {
+    outDir: 'dist', // This must match vercel.json's distDir
+  },
   server: {
     proxy: {
       // This rule proxies any request that starts with "/api"
